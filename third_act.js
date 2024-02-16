46b45b3e80e3b650f415a80d7d316d635e904263
Dremin-N <dr.kolka19@gmail.com>
2024-02-16 16:57:47 +0500

    Task 2.10

--- third_act.js
+++ third_act.js
@@ -33,45 +33,6 @@ if (firstDiv.includes(openDiv) && firstDiv.includes(closeDiv)) {
   console.log("false");
 }
 
-//! Задача 2.1
-//* Метод 1
-const values = ["Строка", true, "Число", "Объект", "Не число", false];
-
-// for (let i = 0; i < values.length; i++) {
-//   if (values[i] === true || values[i] === false) {
-//     values[i] = "Булевый тип";
-//     break;
-//   }
-// }
-// console.log(values);
-
-//* Метод 2
-
-for (let i = 0; i < values.length; i++) {
-  if (values[i] === true || values[i] === false) {
-    values.splice(i, 1, "Булевый тип");
-    break;
-  }
-}
-console.log(values);
-
-// Метод слайс
-let example = values.slice(1, 5);
-console.log(example);
-
-//! Задача 2.2
-const partNumbers = ["ER1234COM", "FIV987451ru", "GE123JO", "P4321NO"];
-//? Через регулярные выражения
-const goodNumbers = [];
-let regexp = /\d\d\w\w/i;
-for (let i = 0; i < partNumbers.length; i++) {
-  let string = partNumbers[i].slice(-4);
-  if (string.search(regexp) !== -1) {
-    goodNumbers.push(partNumbers[i]);
-  }
-}
-console.log(goodNumbers);
-
 //! Задача 1.4
 
 let road = "15 km";
@@ -128,19 +89,6 @@ console.log(ticket.match(/\w\w-\d\d\d\d\d\d\d\d/).join(""));
 console.log(ticket.match(/\w\w-\d\d\d\d\d\d\d\d\d\d\d\d/));
 
 //! Задача 2.1
-
-const values = ["Строка", true, "Число", "Объект", "Не число", false];
-for (let val of values) {
-  console.log(val);
-  if (val === true || val === false) {
-    val = "Булевый тип";
-    console.log(val);
-    break;
-  }
-}
-console.log(values);
-
-//! Задача 2.1
 //* Метод 1
 const values = ["Строка", true, "Число", "Объект", "Не число", false];
 
@@ -178,3 +126,103 @@ for (let i = 0; i < partNumbers.length; i++) {
   }
 }
 console.log(goodNumbers);
+
+//! Задача 2.5 Сделал так чтобы не было ошибок при любом положении и при отсутствии булевого значения
+
+const values_list = [
+  true,
+  "Строка",
+  "Числа",
+  true,
+  "Объект",
+  "Не число",
+  false,
+  "Не объект",
+];
+let min;
+let max;
+let result = [];
+let find_true = values_list.findIndex((val) => val === true);
+let find_false = values_list.findIndex((val) => val === false);
+
+if (find_true !== -1 && find_false === -1) {
+  min = find_true;
+} else if (find_false !== -1 && find_true === -1) {
+  min = find_false;
+} else if (find_false < find_true) {
+  min = find_false;
+} else if (find_true < find_false) {
+  min = find_true;
+} else {
+  min = -1;
+}
+console.log(min);
+find_true = values_list.lastIndexOf(true);
+find_false = values_list.lastIndexOf(false);
+
+if (find_true > find_false) {
+  max = find_true;
+} else if (find_false > find_true) {
+  max = find_false;
+} else {
+  max = values_list.length - 1;
+}
+
+if (min === max) {
+  max = values_list.length - 1;
+}
+
+if (min !== -1) {
+  for (let i = min; i <= max; i++) {
+    result.push(values_list[i]);
+  }
+} else {
+  result = "Нет булевого значения";
+}
+
+console.log(result);
+
+//! Задача 2.6 В программе задана переменная values, которая хранит в себе массив. Элементы массива являются числами. Определите, находятся ли в заданном массиве элементы, которые содержат четырехзначные числа. Результат выведите в консоль в виде булевого значения (true, если содержит, и false, если нет). + Задача 2.7
+
+let values_numbers = [10, 3032, 185, 11200, 980, 1002];
+
+const four_digits = values_numbers.some((val) => val >= 1000 && val <= 9999);
+let reg = /\d\d\d\d/;
+if (four_digits === false) {
+  console.log("Искомый элемент не был найден");
+} else {
+  let index = values_numbers.findIndex((num) => num >= 1000 && num <= 9999);
+  console.log(index);
+}
+
+//! ЗАДАЧА 2.8 В программе задана переменная users, которая хранит в себе массив. Элементы данного массива являются объектами. Определите индекс элемента, значение свойства role которого содержит больше одного слова. Результат выведите в консоль разработчика.
+
+let users_1 = [
+  { login: "user1", role: "Admin" },
+  { login: "user2", role: "Stateuser" },
+  { login: "user3", role: "Moderator" },
+];
+let index_2 = users_1.findIndex((val) => val.role.split(" ").length === 2);
+console.log(index_2);
+
+//! Задача 2.9 В программе задана переменная array, которая хранит в себе двумерный массив. Его элементы — массивы, состоящие из чисел. Отсортируйте массив по количеству элементов вложенного массива по возрастанию. Результат выведите в консоль.
+
+let array = [
+  [1, 2, 3],
+  [1, 2],
+  [1, 2, 3, 4],
+];
+
+const sortBylength = (a, b) => {
+  const aLength = a.length;
+  const bLength = b.length;
+  if (aLength == bLength) return 0;
+  if (aLength < bLength) return -1;
+  if (aLength > bLength) return 1;
+};
+
+array.sort(sortBylength);
+
+console.log(array);
+
+//! Задача 2.10 В программе задана переменная randValues, которая хранит в себе массив. Его элементы могут являться как строковыми, так и числовыми значениями. Отсортируйте массив следующим образом: В начале массива должны быть все числовые значения по возрастанию В конце все строковые, упорядоченные по алфавиту Результат выведите в консоль.
