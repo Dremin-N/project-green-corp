d0154e6ddcf4cd338f4ae5a65657fe4cb532cd7e
Dremin-N <dr.kolka19@gmail.com>
2024-03-13 22:47:30 +0500

    dads

--- js/animations.js
+++ js/animations.js
@@ -58,3 +58,20 @@ function updateScroll(event) {
 }
 
 window.addEventListener("scroll", updateScroll);
+
+function addSmoothScroll(link) {
+  link.addEventListener("click", onLinkClick);
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  document.querySelector(event.target.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+document.querySelectorAll("a[href^='#']").forEach((el) => {
+  addSmoothScroll(el);
+});
+
+addSmoothScroll(document.querySelector(".more-button"));
